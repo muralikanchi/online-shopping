48bf9301577ddf63b466304c9a8fb94cc6f9a6c8
murali <muralikanchi@yahoo.com>
2017-08-14 17:20:19 +0530

    Successfully loaded the static Resources

---
+++ onlineshopping/src/main/java/com/murali/onlineshopping/controller/PageController.java
@@ -0,0 +1,40 @@
+package com.murali.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+		@RequestMapping(value={"/","/index","/home"})
+		public ModelAndView index(){
+			ModelAndView mv = new ModelAndView("page");
+			mv.addObject("greeting", "Welcome to Spring Web MVC");
+			return mv;
+		}
+		
+	/** Commented below is meant for practice and not part of the application*/
+		
+//		@RequestMapping(value="/test")
+//		public ModelAndView test(@RequestParam(
+//				value="greeting", required=false)String greeting){
+//			if(greeting==null){
+//				greeting = "Hello there!";
+//			}
+//			ModelAndView mv = new ModelAndView("page");
+//			mv.addObject("greeting", greeting);
+//			return mv;
+//		}
+
+//		@RequestMapping("/test/{greeting}")
+//		public ModelAndView test(@PathVariable(
+//				value="greeting", required=false) String greeting){
+//			if(greeting == null){
+//				greeting = "Hi there!";
+//			}
+//			ModelAndView mv = new ModelAndView("page");
+//			mv.addObject("greeting", greeting);
+//			return mv;
+//		}
+}
