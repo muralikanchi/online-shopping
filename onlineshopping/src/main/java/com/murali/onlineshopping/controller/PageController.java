817078c48d2729866e2d4963d1e3d2207c83134c
murali <muralikanchi@yahoo.com>
2017-08-15 23:35:38 +0530

    Completed the user interface theme and other issues.

--- onlineshopping/src/main/java/com/murali/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/murali/onlineshopping/controller/PageController.java
@@ -10,7 +10,24 @@ public class PageController {
 		@RequestMapping(value={"/","/index","/home"})
 		public ModelAndView index(){
 			ModelAndView mv = new ModelAndView("page");
-			mv.addObject("greeting", "Welcome to Spring Web MVC");
+			mv.addObject("title", "Home");
+			mv.addObject("userClickHome", true);
+			return mv;
+		}
+		
+		@RequestMapping("/about")
+		public ModelAndView about(){
+			ModelAndView mv = new ModelAndView("page");
+			mv.addObject("title", "About Us");
+			mv.addObject("userClickAbout", true);
+			return mv;
+		}
+		
+		@RequestMapping("/contact")
+		public ModelAndView contact(){
+			ModelAndView mv = new ModelAndView("page");
+			mv.addObject("title", "Contact Us");
+			mv.addObject("userClickContact", true);
 			return mv;
 		}
 		
